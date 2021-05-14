1334834afbc1833be108cf5580328638a01ec9c7
Nikolas <nikolasil2000@gmail.com>
2021-05-14 14:24:04 +0300

    good progress on filling tree

--- course/minijava_example/Main.java
+++ course/minijava_example/Main.java
@@ -8,34 +8,35 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // if (args.length != 1) {
-        // System.err.println("Usage: java Main <inputFile>");
-        // System.exit(1);
-        // }
+        if (args.length != 1) {
+            System.err.println("Usage: java Main <inputFile>");
+            System.exit(1);
+        }
 
-        // FileInputStream fis = null;
-        // try {
-        // fis = new FileInputStream(args[0]);
-        // MiniJavaParser parser = new MiniJavaParser(fis);
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(args[0]);
+            MiniJavaParser parser = new MiniJavaParser(fis);
 
-        // Goal root = parser.Goal();
+            Goal root = parser.Goal();
 
-        // System.err.println("Program parsed successfully.");
+            System.err.println("Program parsed successfully.");
 
-        // MyVisitor eval = new MyVisitor();
-        // root.accept(eval, null);
-        // } catch (ParseException ex) {
-        // System.out.println(ex.getMessage());
-        // } catch (FileNotFoundException ex) {
-        // System.err.println(ex.getMessage());
-        // } finally {
-        // try {
-        // if (fis != null)
-        // fis.close();
-        // } catch (IOException ex) {
-        // System.err.println(ex.getMessage());
-        // }
-        // }
+            MyVisitor eval = new MyVisitor();
+            root.accept(eval, null);
+
+        } catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (fis != null)
+                    fis.close();
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
     }
 }
 
@@ -94,7 +95,7 @@ class SymbolTable {
     int insertBodyVariableToMethod(String className, String methName, String varName, String varType) {
         if (this.getClass(className) != null) {
             if (this.getClass(className).getMethod(methName) != null) {
-                this.getClass(className).getMethod(methName).addArgument(varName, varType);
+                this.getClass(className).getMethod(methName).addBodyVariable(varName, varType);
                 return 0;
             }
             System.out.println("Error in class method: " + className + "." + methName + " -- not declared --");
@@ -147,6 +148,16 @@ class SymbolTable {
             return this.classes.get(className);
         return null;
     }
+
+    void print() {
+        System.out.println("-- [START PRINTING] --");
+        for (String name : this.classes.keySet()) {
+            this.classes.get(name).print();
+            System.out.println();
+        }
+        System.out.println("-- [END PRINTING] --");
+    }
+
 }
 
 class ST_Class {
@@ -182,7 +193,7 @@ class ST_Class {
     }
 
     int addAtribute(String atrName, String atrType) {
-        if (this.getAtribute(atrName) == null) {
+        if (this.getAtribute(atrName) == "") {
             this.atributes.put(atrName, atrType);
             return 0;
         }
@@ -197,6 +208,31 @@ class ST_Class {
         }
         System.out.println("Error in class method: " + this.name + "." + methName + " -- double declaration --");
         return 1;
+    }
+
+    void print() {
+        if (extend != null)
+            System.out.println("class " + name + " extends: " + extend.getName());
+        else
+            System.out.println("class " + name + " extends: none");
+
+        System.out.println("\t -- [Attributes] --");
+        if (this.atributes.size() == 0)
+            System.out.println("\tnone");
+        else {
+            for (String name : this.atributes.keySet()) {
+                System.out.println("\t" + this.atributes.get(name) + " " + name);
+            }
+        }
+        System.out.println("\n\t -- [Methods] --");
+        if (this.methods.size() == 0)
+            System.out.println("\tnone");
+        else {
+            for (String name : this.methods.keySet()) {
+                System.out.print("\t");
+                this.methods.get(name).print();
+            }
+        }
     }
 }
 
@@ -233,7 +269,7 @@ class ST_Method {
     }
 
     int addArgument(String argName, String argType) {
-        if (this.getArgument(argName) == null) {
+        if (this.getArgument(argName) == "") {
             this.arguments.put(argName, argType);
             return 0;
         }
@@ -242,7 +278,7 @@ class ST_Method {
     }
 
     int addBodyVariable(String varName, String varType) {
-        if (this.getBodyVariable(varName) == null) {
+        if (this.getBodyVariable(varName) == "") {
             this.bodyVariables.put(varName, varType);
             return 0;
         }
@@ -250,9 +286,25 @@ class ST_Method {
                 "Error in method's bodyVariables : " + this.name + "." + varName + " -- double declaration --");
         return 1;
     }
+
+    void print() {
+        System.out.print(type + " " + name + "(");
+
+        for (String name : this.arguments.keySet()) {
+            System.out.print(this.arguments.get(name) + " " + name + ",");
+        }
+        System.out.println(")");
+        System.out.print("\t\t\tbodyVariables: \n\t\t\t  ");
+        for (String name : this.bodyVariables.keySet()) {
+            System.out.print(this.bodyVariables.get(name) + " " + name + ",");
+        }
+        System.out.println("\n");
+    }
 }
 
-class MyVisitor extends GJDepthFirst<String, Void> {
+class MyVisitor extends GJDepthFirst<String, String> {
+    static SymbolTable ST = new SymbolTable();
+
     /**
      * f0 -> "class" f1 -> Identifier() f2 -> "{" f3 -> "public" f4 -> "static" f5
      * -> "void" f6 -> "main" f7 -> "(" f8 -> "String" f9 -> "[" f10 -> "]" f11 ->
@@ -260,11 +312,20 @@ class MyVisitor extends GJDepthFirst<String, Void> {
      * Statement() )* f16 -> "}" f17 -> "}"
      */
     @Override
-    public String visit(MainClass n, Void argu) throws Exception {
+    public String visit(MainClass n, String argu) throws Exception {
+        System.out.println("[MainClass]");
         String classname = n.f1.accept(this, null);
         System.out.println("Class: " + classname);
 
-        super.visit(n, argu);
+        ST.enter(classname, null);
+
+        String arg = n.f11.accept(this, null);
+        super.visit(n, classname);
+
+        ST.insertMethod(classname, "main", "void");
+        ST.insertArgumentToMethod(classname, "main", arg, "String[]");
+
+        n.f14.accept(this, classname + "->" + "main");
 
         System.out.println();
 
@@ -276,12 +337,16 @@ class MyVisitor extends GJDepthFirst<String, Void> {
      * ( MethodDeclaration() )* f5 -> "}"
      */
     @Override
-    public String visit(ClassDeclaration n, Void argu) throws Exception {
+    public String visit(ClassDeclaration n, String argu) throws Exception {
+        System.out.println("[ClassDeclaration]");
         String classname = n.f1.accept(this, null);
         System.out.println("Class: " + classname);
 
-        super.visit(n, argu);
+        if (ST.enter(classname, null) != 0)
+            System.exit(1);
 
+        super.visit(n, classname);
+        ST.print();
         System.out.println();
 
         return null;
@@ -292,13 +357,58 @@ class MyVisitor extends GJDepthFirst<String, Void> {
      * f5 -> ( VarDeclaration() )* f6 -> ( MethodDeclaration() )* f7 -> "}"
      */
     @Override
-    public String visit(ClassExtendsDeclaration n, Void argu) throws Exception {
+    public String visit(ClassExtendsDeclaration n, String argu) throws Exception {
+        System.out.println("[ClassExtendsDeclaration]");
         String classname = n.f1.accept(this, null);
+        String extend = n.f3.accept(this, null);
         System.out.println("Class: " + classname);
 
-        super.visit(n, argu);
+        if (ST.enter(classname, extend) != 2)
+            System.exit(1);
+
+        super.visit(n, classname);
 
         System.out.println();
+
+        return null;
+    }
+
+    /**
+     * f0 -> Type() f1 -> Identifier() f2 -> ";"
+     */
+    @Override
+    public String visit(VarDeclaration n, String argu) throws Exception {
+        System.out.println("[VarDeclaration]");
+        String type = n.f0.accept(this, argu);
+        String name = n.f1.accept(this, argu);
+        String[] a = argu.split("->");
+        for (String o : a) {
+            System.out.println(o);
+        }
+        System.out.println("l " + a.length);
+        if (a.length == 1) {
+            n.f2.accept(this, argu);
+            System.out.println(type + " " + name);
+            ST.insertAtribute(argu, name, type);
+        } else if (a.length == 2) {
+            String classname = "";
+            String methname = "";
+            int k = 1;
+            for (int i = 0; i < a.length; i++) {
+                // System.out.println(a[i]);
+                if (k == 1) {
+                    classname = a[i];
+                } else {
+                    methname = a[i];
+                }
+                if (a[i].length() != 0)
+                    k = 2;
+            }
+            System.out.println("here " + classname + " " + methname);
+            ST.insertBodyVariableToMethod(classname, methname, name, type);
+        } else {
+            System.out.println("WTF???????????????????");
+        }
 
         return null;
     }
@@ -309,13 +419,39 @@ class MyVisitor extends GJDepthFirst<String, Void> {
      * -> ( Statement() )* f9 -> "return" f10 -> Expression() f11 -> ";" f12 -> "}"
      */
     @Override
-    public String visit(MethodDeclaration n, Void argu) throws Exception {
+    public String visit(MethodDeclaration n, String argu) throws Exception {
+        System.out.println("[MethodDeclaration]");
         String argumentList = n.f4.present() ? n.f4.accept(this, null) : "";
 
         String myType = n.f1.accept(this, null);
         String myName = n.f2.accept(this, null);
-
+        ST.insertMethod(argu, myName, myType);
+        String[] arguments = argumentList.split(",");
+        for (String arg : arguments) {
+            // System.out.println("1");
+            String[] a = arg.split("\\s");
+            String a1 = "";
+            String a2 = "";
+            int k = 1;
+            for (int i = 0; i < a.length; i++) {
+                // System.out.println(a[i]);
+                if (k == 1) {
+                    a1 = a[i];
+                } else {
+                    a2 = a[i];
+                }
+                if (a[i].length() != 0)
+                    k = 2;
+            }
+            // System.out.println(a1 + " " + a2);
+            if (k == 2)
+                ST.insertArgumentToMethod(argu, myName, a2, a1);
+        }
         System.out.println(myType + " " + myName + " -- " + argumentList);
+        System.out.println("beforenik " + myName);
+
+        super.visit(n, argu + "->" + myName);
+        System.out.println("after");
         return null;
     }
 
@@ -323,7 +459,8 @@ class MyVisitor extends GJDepthFirst<String, Void> {
      * f0 -> FormalParameter() f1 -> FormalParameterTail()
      */
     @Override
-    public String visit(FormalParameterList n, Void argu) throws Exception {
+    public String visit(FormalParameterList n, String argu) throws Exception {
+        System.out.println("[FormalParameterList]");
         String ret = n.f0.accept(this, null);
 
         if (n.f1 != null) {
@@ -336,7 +473,8 @@ class MyVisitor extends GJDepthFirst<String, Void> {
     /**
      * f0 -> FormalParameter() f1 -> FormalParameterTail()
      */
-    public String visit(FormalParameterTerm n, Void argu) throws Exception {
+    public String visit(FormalParameterTerm n, String argu) throws Exception {
+        System.out.println("[FormalParameterTerm]");
         return n.f1.accept(this, argu);
     }
 
@@ -344,7 +482,8 @@ class MyVisitor extends GJDepthFirst<String, Void> {
      * f0 -> "," f1 -> FormalParameter()
      */
     @Override
-    public String visit(FormalParameterTail n, Void argu) throws Exception {
+    public String visit(FormalParameterTail n, String argu) throws Exception {
+        System.out.println("[FormalParameterTail]");
         String ret = "";
         for (Node node : n.f0.nodes) {
             ret += ", " + node.accept(this, null);
@@ -357,27 +496,32 @@ class MyVisitor extends GJDepthFirst<String, Void> {
      * f0 -> Type() f1 -> Identifier()
      */
     @Override
-    public String visit(FormalParameter n, Void argu) throws Exception {
+    public String visit(FormalParameter n, String argu) throws Exception {
+        System.out.println("[FormalParameter]");
         String type = n.f0.accept(this, null);
         String name = n.f1.accept(this, null);
         return type + " " + name;
     }
 
     @Override
-    public String visit(ArrayType n, Void argu) {
+    public String visit(ArrayType n, String argu) {
+        System.out.println("[ArrayType]");
         return "int[]";
     }
 
-    public String visit(BooleanType n, Void argu) {
+    public String visit(BooleanType n, String argu) {
+        System.out.println("[BooleanType]");
         return "boolean";
     }
 
-    public String visit(IntegerType n, Void argu) {
+    public String visit(IntegerType n, String argu) {
+        System.out.println("[IntegerType]");
         return "int";
     }
 
     @Override
-    public String visit(Identifier n, Void argu) {
+    public String visit(Identifier n, String argu) {
+        System.out.println("[Identifier]");
         return n.f0.toString();
     }
 }
